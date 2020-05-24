697afd1299502f959ae319277d46a5151fb119c3
venkat <kumar.cbl@gmail.com>
2020-05-24 17:16:39 +0530

    final prod

--- src/main/java/com/ibm/JavaApp/App.java
+++ src/main/java/com/ibm/JavaApp/App.java
@@ -1,13 +1,76 @@
 package com.ibm.JavaApp;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    
+	public static int firstNum;
+	public static int secondNum;
+	
+	
+	public App(int firstNum2, int secondNum2) {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public int getFirstNum() {
+		return firstNum;
+	}
+
+
+	public void setFirstNum(int firstNum) {
+		this.firstNum = firstNum;
+	}
+
+
+	public int getSecondNum() {
+		return secondNum;
+	}
+
+
+	public void setSecondNum(int secondNum) {
+		this.secondNum = secondNum;
+	}
+
+	public int addFunction(int firstNum, int secondNum) {
+			return firstNum+secondNum;
+	}
+	
+	public int subFunction(int firstNum, int secondNum) {
+		return secondNum-firstNum;
+	}
+	
+	public int mulFunction(int firstNum, int secondNum) {
+		return firstNum*secondNum;
+	}
+	
+	public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
+		App app = new App(firstNum, secondNum);
+      
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter the first numeric value ");
+        int firstNum =  sc.nextInt();
+        app.setFirstNum(firstNum);
+        
+        System.out.println("Enter the second numeric value ");
+        int secondNum =  sc.nextInt();
+        app.setSecondNum(secondNum);
+        
+       
+        System.out.println("Sum is : "+app.addFunction(secondNum, secondNum));
+        System.out.println("Difference is : "+app.subFunction(firstNum, secondNum));
+        System.out.println("Product is : "+app.mulFunction(firstNum, secondNum));
+        
+        String output = String.format("\n*** Your Calculator ***\n\nFirst: %d\nSecond: %d\n\nSum : %d\nDifference : %d\nProduct : %d\n\n", app.firstNum, app.secondNum, app.addFunction(firstNum, secondNum), app.subFunction(firstNum, secondNum), app.mulFunction(firstNum, secondNum));
+	    System.out.println(output);
+        
     }
 }
