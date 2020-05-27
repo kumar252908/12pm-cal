f9cac842486a7af72d8c575021ef9a8018c06197
kumar252908 <65782497+kumar252908@users.noreply.github.com>
2020-05-27 22:58:48 +0530

    Update App.java

--- src/main/java/com/ibm/JavaApp/App.java
+++ src/main/java/com/ibm/JavaApp/App.java
@@ -6,66 +6,50 @@ import java.util.Scanner;
  * Hello world!
  *
  */
-public class App 
-{
-    
-	public static int firstNum;
-	public static int secondNum;
+public class Calculator {
+
+	public long first;
+	public long second;
 	
-	
-	public App(int firstNum2, int secondNum2) {
-		// TODO Auto-generated constructor stub
-	}
-
-
-	public int getFirstNum() {
-		return firstNum;
-	}
-
-
-	public void setFirstNum(int firstNum) {
-		this.firstNum = firstNum;
-	}
-
-
-	public int getSecondNum() {
-		return secondNum;
-	}
-
-
-	public void setSecondNum(int secondNum) {
-		this.secondNum = secondNum;
-	}
-
-	public int addFunction(int firstNum, int secondNum) {
-			return firstNum+secondNum;
+	public Calculator(long first, long second){
+		this.first = first;
+		this.second=second;
 	}
 	
-	public int subFunction(int firstNum, int secondNum) {
-		return secondNum-firstNum;
+	public long getFirst() {
+		return first;
 	}
-	
-	public int mulFunction(int firstNum, int secondNum) {
-		return firstNum*secondNum;
+
+	public long getSecond() {
+		return second;
 	}
-	
-	public static void main( String[] args )
-    {
-        
+
+	public long addFucn(long first, long second){
 		
-		int firstNum =int.parseint(args[0]);	       
-		int secondNum =int.parseint(args[1]);
-		App app = new App(firstNum, secondNum);
-      
-               
-        
-       
-        System.out.println("Sum is : "+app.addFunction(firstNum, secondNum));
-        System.out.println("Difference is : "+app.subFunction(firstNum, secondNum));
-        System.out.println("Product is : "+app.mulFunction(firstNum, secondNum));
-        System.out.println("build is completed");
-        String output = String.format("\n*** Your Calculator ***\n\nFirst: %d\nSecond: %d\n\nSum : %d\nDifference : %d\nProduct : %d\n\n", app.firstNum, app.secondNum, app.addFunction(firstNum, secondNum), app.subFunction(firstNum, secondNum), app.mulFunction(firstNum, secondNum));
+		return first+second;
+	}
+	
+	public long subFucn(long first, long second){
+		
+		return second-first;
+	}
+	
+	public long mulFucn(long first, long second){
+		
+		return first*second;
+	}
+	
+	
+	public static void main(String[] args) {
+
+		long first = Long.parseLong(args[0]);
+		long second = Long.parseLong(args[1]);
+		
+		Calculator cal = new Calculator(first, second);
+		
+		String output = String.format("\n*** Your Results ***\n\nFirst: %d\nSecond: %d\n\nSum : %d\nDifference : %d\nProduct : %d\n\n", cal.first, cal.second, cal.addFucn(first, second), cal.subFucn(first, second), cal.mulFucn(first, second));
+	    System.out.println("CALCULATOR DISPLAY");
 	    System.out.println(output);
-        
-    }
+	}
+	
 }
